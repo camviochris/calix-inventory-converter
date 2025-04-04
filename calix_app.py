ce6142a385deda5b2982e690ed205be9df0aa8fb
camviochris <chris@camv.io>
2025-04-04 13:19:31 -0500

    Update calix_app.py

--- calix_app.py
+++ calix_app.py
@@ -56,7 +56,7 @@ with st.expander("📁 Step 1: Upload File", expanded=not st.session_state.heade
                 df.columns = df.columns.str.strip()
                 st.session_state.df = df
                 st.session_state.header_confirmed = True
-                sst.rerun()
+                st.rerun()
         except Exception as e:
             st.error(f"Error reading file: {e}")
 
