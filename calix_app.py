cb7efe6e5c4174c31656f4993657a8a5e72c5fc2
camviochris <chris@camv.io>
2025-04-04 00:08:18 -0500

    same error update

--- calix_app.py
+++ calix_app.py
@@ -138,6 +138,7 @@ with step2_expander:
         st.markdown("---")
         if "company_name" not in st.session_state:
     st.session_state.company_name = ""
+
 step3_expander = st.expander("📦 Step 3: Export Setup", expanded=not st.session_state.get("export_complete", False))
 with step3_expander:
         st.session_state.company_name = st.text_input("Enter your company name", value=st.session_state.company_name, help="This will be used to name the output file.")
@@ -229,4 +230,4 @@ with step3_expander:
 
 # Footer
 st.markdown("---")
-st.markdown("<div style='text-align: right; font-size: 0.75em; color: gray;'>Last updated: 2025-04-03 • Rev: v2.41</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: right; font-size: 0.75em; color: gray;'>Last updated: 2025-04-03 • Rev: v2.42</div>", unsafe_allow_html=True)
